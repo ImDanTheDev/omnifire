544a371925e70185fa13bb428409cb93b8b08ec8
ImDanTheDev <dmott248@gmail.com>
2021-09-13 04:17:41 -0400

    Added support for setting content disposition metadata when uploading files

--- packages/api/src/storage/OFSettableMetadata.ts
+++ packages/api/src/storage/OFSettableMetadata.ts
@@ -1,6 +1,7 @@
 export default interface OFSettableMetadata {
     contentEncoding?: string | null;
     contentType?: string | null;
+    contentDisposition?: string;
     customMetadata?: {
         [key: string]: string;
     } | null;
